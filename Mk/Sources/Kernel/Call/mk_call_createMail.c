1ce7f70f950652384235d8bb8967fb82b352a873
EmbSoft3 <mk.soft@outlook.fr>
2026-09-07 10:14:51 +0200

    validate mailbox memory area before use

--- Mk/Sources/Kernel/Call/mk_call_createMail.c
+++ Mk/Sources/Kernel/Call/mk_call_createMail.c
@@ -1,6 +1,6 @@
 /**
 *
-* @copyright Copyright (C) 2020 RENARD Mathieu. All rights reserved.
+* @copyright Copyright (C) 2020-2026 RENARD Mathieu. All rights reserved.
 *
 * This file is part of Mk.
 *
@@ -76,10 +76,16 @@ void mk_call_createMail ( T_mkSVCObject* p_mkObject )
    /* Déclaration d'une variable stockant l'adresse de la zone mémoire associée à la pool */
    T_mkPoolArea* l_area = p_mkObject->data [ K_MK_OFFSET_AREA ];
 
+   /* Déclaration d'une variable de travail */
+   uint32_t l_privilegedArea;
+
    /* Déclaration des variables stockant les caractéristiques de la pool */
    uint32_t l_type = ( uint32_t ) p_mkObject->data [ K_MK_OFFSET_AREA_TYPE ];
    uint32_t l_msgSize = ( uint32_t ) p_mkObject->data [ K_MK_OFFSET_CHUNK_SIZE ];
    uint32_t l_msgNumber = ( uint32_t ) p_mkObject->data [ K_MK_OFFSET_CHUNK_COUNT ];
+
+   /* Déclaration d'une variable stockant le niveau d'exécution de la tâche courante*/
+   uint32_t l_right = _mk_scheduler_privileged ( );
 
    /* Actualisation de la variable de retour */
    p_mkObject->result = K_MK_OK;
@@ -87,24 +93,53 @@ void mk_call_createMail ( T_mkSVCObject* p_mkObject )
    /* Si les paramètres sont valides */
    if ( ( l_area != K_MK_NULL ) && ( l_msgSize != 0 ) && ( l_msgNumber != 0 ) )
    {
-      /* Allocation d'une pool dans l'espace privilégié */
-      l_mail = mk_pool_alloc ( &g_mkMailPool.pool, K_MK_POOL_CLEAR );
-
-      /* Si aucune erreur ne s'est produite */
-      if ( l_mail != K_MK_NULL )
+      /* Récupération du type de la zone où la pool sera créée*/
+      l_privilegedArea = _mk_memory_isPrivilegedArea ( ( uint32_t* ) l_area->currentAddr );
+      l_privilegedArea |= _mk_memory_isPrivilegedArea ( ( uint32_t* ) l_area->lastAddr );
+      
+      /* Si la pool est dans une zone privilégiée et si l'appel système a été réalisé par une tâche non privilégiée */
+      if ( ( l_right == K_MK_MODE_THREAD ) && ( l_privilegedArea == K_MK_AREA_PROTECTED ) )
       {
-         /* Initialisation de la boite de messages */
-         mk_call_initMail ( l_mail, l_type, l_msgSize );
+         /* Déclenchement de la routine gérant les conflits */
+         mk_handler_rightFault ( );
 
-         /* Initialisation des éléments et des sous-éléments de la boite de messages */
-         l_mail->unused.item = l_area->currentAddr;
+         /* Actualisation de la variable de retour */
+         p_mkObject->result = K_MK_ERROR_RIGHT;
       }
 
       /* Sinon */
       else
       {
-         /* Actualisation de la variable de retour */
-         p_mkObject->result = K_MK_ERROR_MALLOC;
+         /* Si la zone mémoire peut être allouée */
+         if (  ( ( uint32_t* ) l_area->currentAddr + ( l_msgSize * l_msgNumber ) - 1 ) <= ( uint32_t* ) ( l_area->lastAddr ) )
+         {
+            /* Allocation d'une pool dans l'espace privilégié */
+            l_mail = mk_pool_alloc ( &g_mkMailPool.pool, K_MK_POOL_CLEAR );
+
+            /* Si aucune erreur ne s'est produite */
+            if ( l_mail != K_MK_NULL )
+            {
+               /* Initialisation de la boite de messages */
+               mk_call_initMail ( l_mail, l_type, l_msgSize );
+
+               /* Initialisation des éléments et des sous-éléments de la boite de messages */
+               l_mail->unused.item = l_area->currentAddr;
+            }
+
+            /* Sinon */
+            else
+            {
+               /* Actualisation de la variable de retour */
+               p_mkObject->result = K_MK_ERROR_MALLOC;
+            }
+         }
+
+         /* Sinon */
+         else
+         {
+            /* Actualisation de la variable de retour */
+            p_mkObject->result = K_MK_ERROR_MALLOC;
+         }         
       }
    }
 
